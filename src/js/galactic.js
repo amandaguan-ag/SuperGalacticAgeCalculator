cc3b316bb2f916f759bce105ddeb6effeadd6596
amandaguan-ag <amandaguan1314@gmail.com>
2023-03-10 10:52:32 -0800

    pass galactic constructor test

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -1,4 +1,9 @@
 export default class Galactic {
   constructor(earth, mercury, venus, mars, jupiter) {
+    this.earth = earth;
+    this.mercury = mercury;
+    this.venus = venus;
+    this.mars = mars;
+    this.jupiter = jupiter;
   }
 }
