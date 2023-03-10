a1d225609e41f9613b07c69b4245fb2555999d5a
amandaguan-ag <amandaguan1314@gmail.com>
2023-03-10 11:30:26 -0800

    upload good fail for mercury age

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -16,5 +16,9 @@ describe("Galactic ", () => {
       const galactic = new Galactic(true, false, false, false, false);
       expect(galactic.getAge(21)).toEqual(21);
     });
+    it("should test that age can be converted to Mercury age", function () {
+      const galactic = new Galactic(false, true, false, false, false);
+      expect(galactic.getAge(21)).toEqual(87);
+    });
   });
 });
